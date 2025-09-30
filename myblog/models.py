80fde3eff410f0f7d7e19b7efafe1bc32c2c2510
MariyaKuznetsova <piratca93@mail.ru>
2025-09-30 20:25:11 +0400

    homework_25

--- myblog/models.py
+++ myblog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Record(models.Model):
     title = models.CharField(max_length=150, verbose_name="Заголовок")
-    contents = models.CharField(max_length=150, verbose_name="Содержимое")
+    contents = models.TextField(verbose_name="Содержимое")
     image = models.ImageField(upload_to="images/", blank=True, null=True, verbose_name="Изображение")
     is_publication = models.BooleanField(default=True)
-    number_of_views = models.IntegerField()
+    number_of_views = models.PositiveIntegerField()
     created_at = models.DateField(auto_now_add=True)
 
     class Meta:
