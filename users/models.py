6c407a83cf78f55532766cb5b5934ec62ab44e67
MariyaKuznetsova <piratca93@mail.ru>
2025-10-11 00:56:41 +0400

    homework_27

--- users/models.py
+++ users/models.py
@@ -9,6 +9,8 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True)
     country = models.CharField(max_length=20, blank=True, null=True, verbose_name="Страна")
 
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.views import LogoutView
-from django.urls import reverse_lazy
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.template.context_processors import request
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -11,8 +16,22 @@ class UserCreateView(CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет, перейдите по ссылки для подтверждение почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
 
-class UserLogoutView(LogoutView):
-    next_page = 'users/login'
-    # def get_next_page(self):
-    #     return reverse_lazy('users:login')
+
+def email_verification(request, token):
+    user = get_object_or_404(token=token)
+    user.is_active = True
+    return redirect(reverse('users:login'))
