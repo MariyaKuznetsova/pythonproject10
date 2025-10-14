a24e03c0b44275c2bec7b840472871f3f399ed24
MariyaKuznetsova <piratca93@mail.ru>
2025-10-14 23:25:29 +0400

    homework_27

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "phone_number", "country")
+    list_filter = ("email",)
+    search_fields = ("email", "phone_number")
